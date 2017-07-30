dd10ab3126d4be3f0e328f9cdefd496bb2136f90
Sean O'Connor <oconnorjamessean@gmail.com>
2017-07-30 16:43:40 +0100

    Created getter and setters for new Pokemon class

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,36 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Sean O'Connor on 30/07/2017.
+//  Copyright © 2017 Boiz. All rights reserved.
+//
+
+import Foundation
+
+
+//class that will hold all pokemon
+class Pokemon {
+    
+    //unwrapped because we know it will exist
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        
+        self._name = name
+        self._pokedexID = pokedexID
+        
+    }
+    
+    
+}
