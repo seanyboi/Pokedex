4b73f8b5bf8f58fb0e3842a87c7ceaa45646a781
Sean O'Connor <oconnorjamessean@gmail.com>
2017-08-01 14:17:42 +0100

    Hooked up new outlets to new controller

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -12,18 +12,34 @@ class PokemonDetailVC: UIViewController {
     
     //variable it can be sent into
     var pokemon: Pokemon!
-
+    
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        nameLbl.text = pokemon.name
+        nameLbl.text = pokemon.name.capitalized
 
     
         
     }
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
 
 }
